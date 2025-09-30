0b5a2278d61b1c77d189341e716ac8d40a99520e
Haiito92 <hannaantoinebusiness@gmail.com>
2025-09-30 17:54:12 +0200

    [GP] Handle drag from different sources

--- Editor/LittleFavoritesTreeView.cs
+++ Editor/LittleFavoritesTreeView.cs
@@ -157,17 +157,29 @@ namespace HaiitoCorp.LittleFavorites.Editor
             }
 
             DragAndDrop.objectReferences = draggedObjects;
-            
+            DragAndDrop.SetGenericData("LittleFavoritesTreeView", this);
             DragAndDrop.StartDrag("Start Favorites Drag.");
         }
 
         protected override DragAndDropVisualMode HandleDragAndDrop(DragAndDropArgs args)
         {
             //Here add more logic to drop in the future (handling of type of objects, folders, etc...)
+
+            
             
             if (args.performDrop)
             {
-                LittleFavoritesEditorData.AddFavorites(DragAndDrop.objectReferences);
+                object origin = DragAndDrop.GetGenericData("LittleFavoritesTreeView");
+                if (origin == this)
+                {
+                    Debug.Log("Drag from the LittleFavoritesTreeView");
+                }
+                else
+                {
+                    Debug.Log("Drag from another window");
+
+                    LittleFavoritesEditorData.AddFavorites(DragAndDrop.objectReferences);
+                }                
             }
             
             return DragAndDropVisualMode.Move;
